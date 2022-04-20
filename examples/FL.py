ed5c73887d1f098d94bd6e511ba0f6fae40b189c
John Mansfield <jlm429@gmail.com>
2022-04-20 04:05:44 -0400

    fix ql env space

--- examples/FL.py
+++ examples/FL.py
@@ -6,7 +6,7 @@ from bettermdptoolbox.RL import QLearner as QL
 from bettermdptoolbox.Planning import Value_Iteration as VI
 
 env = gym.make('FrozenLake8x8-v1')
-Q, V, pi, Q_track, pi_track = QL().q_learning(env=env)
+Q, V, pi, Q_track, pi_track = QL().q_learning(env)
 
 #env = gym.make('FrozenLake8x8-v1')
 #V, pi = VI().value_iteration(env.P)
