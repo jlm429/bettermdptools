1a7de6b37a775cfbcc56325143c80486e8220fff
John Mansfield <jlm429@gmail.com>
2022-04-21 21:24:03 -0400

    blackjack cleanup

--- examples/blackjack.py
+++ examples/blackjack.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+Author: John Mansfield
+"""
 
 import gym
 import pygame
@@ -6,28 +9,37 @@ from bettermdptoolbox.RL import QLearner as QL
 import numpy as np
 from bettermdptoolbox.Planning import Value_Iteration as VI
 
-def test_blackjack():
-    testScores = np.full([100], np.nan)
-    for i in range(0, 100):
-        done = False
-        state = env.reset()
-        state = int(f"{state[0]}{state[1]}{int(state[2])}")
-        totalReward = 0
-        while not done:
-            env.render()
-            action = pi(state)
-            #action=np.random.randint(0,2)
-            next_state, reward, done, blah = env.step(action)
-            next_state = int(f"{next_state[0]}{next_state[1]}{int(next_state[2])}")
-            state = next_state
-            totalReward = reward + totalReward
-        testScores[i] = totalReward
-    env.close()
-    print(testScores)
-    print(testScores.sum())
+class Blackjack():
+    def __init__(self):
+        self.env = gym.make('Blackjack-v1')
+        pass
 
-env = gym.make('Blackjack-v1')
-Q, V, pi, Q_track, pi_track = QL().q_learning(env, n_episodes=50000, gamma=.8)
-test_blackjack()
+    def create_transition_matrix(self):
+        P = 0
+        return P
 
+    def test_blackjack(self):
+        testScores = np.full([100], np.nan)
+        for i in range(0, 100):
+            done = False
+            state = self.env.reset()
+            state = int(f"{state[0]}{state[1]}{int(state[2])}")
+            totalReward = 0
+            while not done:
+                self.env.render()
+                action = pi(state)
+                #action=np.random.randint(0,2)
+                next_state, reward, done, blah = self.env.step(action)
+                next_state = int(f"{next_state[0]}{next_state[1]}{int(next_state[2])}")
+                state = next_state
+                totalReward = reward + totalReward
+            testScores[i] = totalReward
+        self.env.close()
+        print(testScores)
+        print(testScores.sum())
 
+if __name__=="__main__":
+    #env = gym.make('Blackjack-v1')
+    blackjack = Blackjack()
+    Q, V, pi, Q_track, pi_track = QL().q_learning(blackjack.env, n_episodes=50000, gamma=.8)
+    blackjack.test_blackjack()
