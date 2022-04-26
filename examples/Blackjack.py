fc7bd9479396d75ab9754b826aea132666c8d781
John Mansfield <jlm429@gmail.com>
2022-04-26 07:54:03 -0400

    fixed blackjack state sync bug

--- examples/Blackjack.py
+++ examples/Blackjack.py
@@ -8,6 +8,7 @@ import pygame
 from bettermdptoolbox.RL import QLearner as QL
 import numpy as np
 from bettermdptoolbox.Planning import Value_Iteration as VI
+from bettermdptoolbox.Planning import Policy_Iteration as PI
 import pickle
 
 class Blackjack():
@@ -16,24 +17,35 @@ class Blackjack():
         pass
 
     def create_transition_matrix(self):
-        #Transition probability matrix: https://github.com/rhalbersma/gym-blackjack-v1
+        #Transition probability matrix:
+        #https://github.com/rhalbersma/gym-blackjack-v1
         P = pickle.load( open( "blackjack-envP", "rb" ) )
-        print(type(P))
         return P
 
-    def test_blackjack(self):
-        testScores = np.full([100], np.nan)
-        for i in range(0, 100):
+    def convert_state(self, state):
+        s = 0
+        if state[0] == 0: s = state[1] - 1
+        else:
+            if state[2]:
+                s = state[0] + 6
+            else:
+                s = state[0] - 4
+            s = int(f"{s}{state[1] - 1}")
+        return s
+
+    def test_blackjack(self, pi, n_iters):
+        testScores = np.full([n_iters], np.nan)
+        for i in range(0, n_iters):
             done = False
             state = self.env.reset()
-            state = int(f"{state[0]}{state[1]}{int(state[2])}")
+            state = self.convert_state(state)
             totalReward = 0
             while not done:
                 self.env.render()
                 action = pi(state)
                 #action=np.random.randint(0,2)
-                next_state, reward, done, blah = self.env.step(action)
-                next_state = int(f"{next_state[0]}{next_state[1]}{int(next_state[2])}")
+                next_state, reward, done, info = self.env.step(action)
+                next_state = self.convert_state(next_state)
                 state = next_state
                 totalReward = reward + totalReward
             testScores[i] = totalReward
@@ -42,9 +54,8 @@ class Blackjack():
         print(testScores.sum())
 
 if __name__=="__main__":
-    #env = gym.make('Blackjack-v1')
     blackjack = Blackjack()
     P=blackjack.create_transition_matrix()
-
+    V, pi = VI().value_iteration(P)
     #Q, V, pi, Q_track, pi_track = QL().q_learning(blackjack.env)
-    #blackjack.test_blackjack()
+    blackjack.test_blackjack(pi, 100)
--- examples/FL.py
+++ examples/FL.py
@@ -7,9 +7,9 @@ from bettermdptoolbox.Planning import Value_Iteration as VI
 from bettermdptoolbox.Planning import Policy_Iteration as PI
 
 env = gym.make('FrozenLake8x8-v1')
-#Q, V, pi, Q_track, pi_track = QL().q_learning(env)
+Q, V, pi, Q_track, pi_track = QL().q_learning(env)
 
 #V, pi = VI().value_iteration(env.P)
-V, pi = PI().policy_iteration(env.P)
+#V, pi = PI().policy_iteration(env.P)
 
 
