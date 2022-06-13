70368c8f5af3816bac788045218655c7dc36baf1
John Mansfield <jlm429@gmail.com>
2022-06-13 09:01:16 -0400

    refactor blackjack.py

--- examples/blackjack.py
+++ examples/blackjack.py
@@ -3,10 +3,9 @@
 Author: John Mansfield
 """
 
-#todo
-#fix pep8 issues
-#update readme
-#add callbacks
+# todo
+# update readme
+# add callbacks
 
 import gym
 import pygame
@@ -27,39 +26,41 @@ class Blackjack():
 
     @staticmethod
     def create_transition_matrix():
-        #Transition probability matrix:
-        #https://github.com/rhalbersma/gym-blackjack-v1
-        P = pickle.load( open( "blackjack-envP", "rb" ) )
-        return P
+        # Transition probability matrix:
+        # https://github.com/rhalbersma/gym-blackjack-v1
+        p = pickle.load(open("blackjack-envP", "rb"))
+        return p
 
     def test_blackjack(self, pi, n_iters):
-        testScores = np.full([n_iters], np.nan)
+        test_scores = np.full([n_iters], np.nan)
         for i in range(0, n_iters):
             state, done = self.env.reset(), False
             state = self.convert_state_obs(state, done)
-            totalReward = 0
+            total_reward = 0
             while not done:
                 self.env.render()
                 action = pi(state)
-                #action=np.random.randint(0,2)
+                # action=np.random.randint(0,2)
                 next_state, reward, done, info = self.env.step(action)
                 next_state = self.convert_state_obs(next_state, done)
                 state = next_state
-                totalReward = reward + totalReward
-            testScores[i] = totalReward
+                total_reward = reward + total_reward
+            test_scores[i] = total_reward
         self.env.close()
-        print(testScores)
-        print(testScores.sum())
+        print(test_scores)
+        print(test_scores.sum())
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     blackjack = Blackjack()
+    p = blackjack.create_transition_matrix()
+    n_states = len(p)
+    n_actions = blackjack.env.action_space.n
 
-    #VI
-    #P=blackjack.create_transition_matrix()
-    #V, pi = VI().value_iteration(P)
+    # VI
+    # V, pi = VI().value_iteration(p)
 
-    #Q-learning
+    # Q-learning
     QL = QL(blackjack.env)
-    nS, nA = 290, blackjack.env.action_space.n
-    Q, V, pi, Q_track, pi_track = QL.q_learning(nS, nA, blackjack.convert_state_obs)
+    Q, V, pi, Q_track, pi_track = QL.q_learning(n_states, n_actions, blackjack.convert_state_obs)
     blackjack.test_blackjack(pi, 100)
