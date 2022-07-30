33dc0c8ab7fa7433b629af36b61ead5ddc216f63
John Mansfield <jlm429@gmail.com>
2022-07-30 09:14:16 -0400

    access control cleanup

--- examples/blackjack.py
+++ examples/blackjack.py
@@ -16,32 +16,66 @@ import pickle
 
 class Blackjack:
     def __init__(self):
-        self.env = gym.make('Blackjack-v1')
-        self.convert_state_obs = lambda state, done: (
+        self._env = gym.make('Blackjack-v1')
+        self._convert_state_obs = lambda state, done: (
             -1 if done else int(f"{state[0] + 6}{(state[1] - 2) % 10}") if state[2] else int(
                 f"{state[0] - 4}{(state[1] - 2) % 10}"))
+        # Transitions and rewards matrix from: https://github.com/rhalbersma/gym-blackjack-v1
+        self._P = pickle.load(open("blackjack-envP", "rb"))
+        self._n_actions = self.env.action_space.n
+        self._n_states = len(self._P)
 
-    @staticmethod
-    def create_transition_matrix():
-        # Transition probability matrix:
-        # https://github.com/rhalbersma/gym-blackjack-v1
-        P = pickle.load(open("blackjack-envP", "rb"))
-        return P
+    @property
+    def n_actions(self):
+        return self._n_actions
+
+    @n_actions.setter
+    def n_actions(self, n_actions):
+        self._n_actions = n_actions
+
+    @property
+    def n_states(self):
+        return self._n_states
+
+    @n_states.setter
+    def n_states(self, n_states):
+        self._n_states = n_states
+
+    @property
+    def P(self):
+        return self._P
+
+    @P.setter
+    def P(self, P):
+        self._P = P
+
+    @property
+    def env(self):
+        return self._env
+
+    @env.setter
+    def env(self, env):
+        self._env = env
+
+    @property
+    def convert_state_obs(self):
+        return self._convert_state_obs
+
+    @convert_state_obs.setter
+    def convert_state_obs(self, convert_state_obs):
+        self._convert_state_obs = convert_state_obs
 
 
 if __name__ == "__main__":
     blackjack = Blackjack()
-    P = blackjack.create_transition_matrix()
-    n_states = len(P)
-    n_actions = blackjack.env.action_space.n
 
     # VI/PI
-    # V, pi = VI(P).value_iteration()
-    # V, pi = PI(P).policy_iteration()
+    # V, pi = VI(blackjack.P).value_iteration()
+    # V, pi = PI(blackjack.P).policy_iteration()
 
     # Q-learning
-    QL = QL(blackjack.env)
-    Q, V, pi, Q_track, pi_track = QL.q_learning(n_states, n_actions, blackjack.convert_state_obs)
+    #QL = QL(blackjack.env)
+    #Q, V, pi, Q_track, pi_track = QL.q_learning(blackjack.n_states, blackjack.n_actions, blackjack.convert_state_obs)
 
     test_scores = TestEnv.test_env(env=blackjack.env, pi=pi, user_input=False,
                                    convert_state_obs=blackjack.convert_state_obs)
