13d495be9e7587d0e5de875fc00918ac8daae24f
John Mansfield <jlm429@gmail.com>
2022-12-28 05:12:47 -0500

    using new step API

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     license='New BSD',
     author='John Mansfield',
     author_email='jlm429@gmail.com',
-    packages=["algorithms", "callbacks", "decorators"],
+    packages=["algorithms", "callbacks", "decorators", "examples"],
     install_requires=['gym>=0.26, <=0.26.2', 'pygame', 'numpy', 'tqdm'],
 )
