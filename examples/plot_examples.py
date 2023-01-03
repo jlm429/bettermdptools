2f38579c9afa3a611e9361cf8d1a182e4c112e06
John Mansfield <jlm429@gmail.com>
2023-01-03 05:13:35 -0500

    adding plot examples

---
+++ examples/plot_examples.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import gym
+import pygame
+from algorithms.rl import QLearner as QL
+from algorithms.planning import ValueIteration as VI
+from algorithms.planning import PolicyIteration as PI
+from examples.test_env import TestEnv
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+class PlotExamples:
+    @staticmethod
+    def policy_plot(self):
+        pass
+
+    @staticmethod
+    def state_values_plot(self):
+        pass
+
+    @staticmethod
+    def max_value_v_iters_plot(self):
+        pass
+
+    @staticmethod
+    def max_reward_v_iters_plot(rewards):
+        df = pd.DataFrame(data=rewards)
+        df.columns = ["Max Reward"]
+        sns.set_theme(style="whitegrid")
+        sns.lineplot(x=df.index, y="Max Reward", data=df).set_title('Max Reward v Iterations')
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
+
+    # VI/PI
+    #V, pi = VI(frozen_lake.env.P).value_iteration()
+    #V, pi = PI(frozen_lake.env.P).policy_iteration()
+
+    #Q-learning
+    QL = QL(frozen_lake.env)
+    Q, V, pi, Q_track, pi_track = QL.q_learning()
+
+    max_reward_per_iter = np.amax(np.amax(Q_track, axis=2), axis=1)
+    PlotExamples.max_reward_v_iters_plot(max_reward_per_iter)
