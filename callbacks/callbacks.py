41d4063a9734164f76a46fc112375bb44caaab66
John Mansfield <jlm429@gmail.com>
2023-01-19 05:38:29 -0500

    update docs

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -5,6 +5,9 @@ Author: John Mansfield
 
 
 class Callbacks:
+    """
+    Base class.
+    """
     def __init__(self):
         pass
 
@@ -22,22 +25,47 @@ class Callbacks:
 
 
 class MyCallbacks(Callbacks):
+    """
+    To create a callback, override one of the callback functions in the child class MyCallbacks.
+    """
     def __init__(self):
         pass
 
     def on_episode(self, caller, episode):
+        """
+        Parameters
+        ----------------------------
+        caller (RL type): Calling object
+
+        episode {int}: Current episode from caller
+        """
         # do things on specific episodes
         pass
 
     def on_episode_begin(self, caller):
+        """
+        Parameters
+        ----------------------------
+        caller (RL type): Calling object
+        """
         # do things on episode begin
         pass
 
     def on_episode_end(self, caller):
+        """
+        Parameters
+        ----------------------------
+        caller (RL type): Calling object
+        """
         # do things on episode end
         pass
 
     def on_env_step(self, caller):
+        """
+        Parameters
+        ----------------------------
+        caller (RL type): Calling object
+        """
         # do things on env. step
         pass
     
