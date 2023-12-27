fd11245877b19e7a11d286449cd9e3308067df61
John <jmansfield6@gatech.edu>
2023-12-27 13:35:48 -0500

    update gridsearch/blackjack

---
+++ utils/blackjack_wrapper.py
@@ -0,0 +1,133 @@
+"""
+Author: John Mansfield
+
+Blackjack wrapper that modifies the observation space and creates a transition/reward matrix P.
+
+# Transitions and rewards matrix from: https://github.com/rhalbersma/gym-blackjack-v1
+#    Observations:
+#   There are 29 * 10 = 290 discrete observable states:
+#    29 player hands: H4-H21, S12-S21, BJ (0-28)
+#     H4   =  0
+#     H5   =  1
+#     H6   =  2
+#     H7   =  3
+#     H8   =  4
+#     H9   =  5
+#     H10  =  6
+#     H11  =  7
+#     H12  =  8
+#     H13  =  9
+#     H14  = 10
+#     H15  = 11
+#     H16  = 12
+#     H17  = 13
+#     H18  = 14
+#     H19  = 15
+#     H20  = 16
+#     H21  = 17
+#     S12  = 18
+#     S13  = 19
+#     S14  = 20
+#     S15  = 21
+#     S16  = 22
+#     S17  = 23
+#     S18  = 24
+#     S19  = 25
+#     S20  = 26
+#     S21  = 27
+#     BJ   = 28
+#    Concatenated with 10 dealer cards: 2-9, T, A (0-9)
+#     _2 = 0
+#     _3 = 1
+#     _4 = 2
+#     _5 = 3
+#     _6 = 4
+#     _7 = 5
+#     _8 = 6
+#     _9 = 7
+#     _T = 8 # 10, J, Q, K are all denoted as T
+#     _A = 9
+"""
+
+import gymnasium as gym
+import os
+import pickle
+
+class CustomTransformObservation(gym.ObservationWrapper):
+    def __init__(self, env, func, observation_space):
+        """
+        Parameters
+        ----------------------------
+        env {gymnasium.Env}:
+            Blackjack base environment to be wrapped
+
+        func {lambda}:
+            Function that converts the observation
+
+        observation_space {gymnasium.spaces.Space}:
+            New observation space
+        """
+        super().__init__(env)
+        if observation_space is not None:
+            self.observation_space = observation_space
+        self.func = func
+
+    def observation(self, observation):
+        """
+        Parameters
+        ----------------------------
+        observation {Tuple}:
+            Blackjack base environment tuple
+
+        Returns
+        ----------------------------
+        self.func(observation) {int}
+        """
+        return self.func(observation)
+
+class BlackjackWrapper(gym.Wrapper):
+    def __init__(self, env):
+        """
+        Parameters
+        ----------------------------
+        env {gymnasium.Env}:
+            Blackjack base environment
+
+        Explanation of convert_state_obs lambda:
+        Lambda function assigned to the variable `self._convert_state_obs` takes parameter, `state` and
+        converts the input into a compact single integer value by concatenating player hand with dealer card.
+        See comments above for further information.
+
+        """
+        self._transform_obs = lambda obs: (
+            int(f"{28}{(obs[1] - 2) % 10}") if (obs[0] == 21 and obs[2])
+            else int(f"{27}{(obs[1] - 2) % 10}") if (obs[0] == 21 and not obs[2])
+            else int(f"{obs[0] + 6}{(obs[1] - 2) % 10}") if obs[2]
+            else int(f"{obs[0] - 4}{(obs[1] - 2) % 10}"))
+        env = CustomTransformObservation(env, self._transform_obs, gym.spaces.Discrete(290))
+        super().__init__(env)
+        current_dir = os.path.dirname(__file__)
+        file_name = 'blackjack-envP.pickle'
+        f = os.path.join(current_dir, file_name)
+        try:
+            self._P = pickle.load(open(f, "rb"))
+        except IOError:
+            print("Pickle load failed.  Check path", f)
+
+    @property
+    def P(self):
+        """
+        Returns
+        ----------------------------
+        _P {dict}
+        """
+        return self._P
+
+    @property
+    def transform_obs(self):
+        """
+        Returns
+        ----------------------------
+        _transform_obs {lambda}
+        """
+        return self._transform_obs
--- utils/grid_search.py
+++ utils/grid_search.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from algorithms.rl import RL
+from algorithms.planner import Planner
+from utils.test_env import TestEnv
+import numpy as np
 import itertools
-
 
 class GridSearch:
     @staticmethod
-    def Q_learning_grid_search(env, epsilon_decay, iters):
-        for i in itertools.product(epsilon_decay, iters):
-            print("running -- with epsilon decay: ", i[0],  " iterations: ", i[1])
-            Q, V, pi, Q_track, pi_track = RL(env).q_learning(epsilon_decay_ratio=i[0], n_episodes=i[1])
+    def Q_learning_grid_search(env, gamma, epsilon_decay, iters):
+        for i in itertools.product(gamma, epsilon_decay, iters):
+            print("running Q_learning with gamma:", i[0],  "epsilon decay:", i[1],  " iterations:", i[2])
+            Q, V, pi, Q_track, pi_track = RL(env).q_learning(gamma=i[0], epsilon_decay_ratio=i[1], n_episodes=i[2])
+            episode_rewards = TestEnv.test_env(env=env, n_iters=100, render=False, user_input=False, pi=pi)
+            print("Avg. episode reward: ", np.mean(episode_rewards))
+            print("###################")
+
+    @staticmethod
+    def planner_grid_search(env, gamma, n_iters, theta):
+        for i in itertools.product(gamma, n_iters, theta):
+            print("running planner with gamma:", i[0],  " n_iters:", i[1], " theta:", i[2])
+            V, V_track, pi = Planner(env.P).policy_iteration(gamma=i[0], n_iters=i[1], theta=i[2])
+            episode_rewards = TestEnv.test_env(env, n_iters=100, render=False, user_input=False, pi=pi)
+            print("Avg. episode reward: ", np.mean(episode_rewards))
+            print("###################")
