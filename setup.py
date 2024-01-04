f4fc7f29b790c90e4bbf55f2caac5a861ca9e9f6
John <jmansfield6@gatech.edu>
2024-01-04 17:30:31 -0500

    update version

--- setup.py
+++ setup.py
@@ -8,12 +8,21 @@ if you're facing trouble installing pygame with Python 3.11, you need to install
 
 More discussion:
 https://stackoverflow.com/questions/74188013/python-pygame-not-installing
+
+Conda/jupyter import issues
+
+Make sure the site-packages directory has the correct name. Run the following command to locate the site-packages folder.
+
+python -c "import site; print(site.getsitepackages())"
+
+Also see:
+https://stackoverflow.com/questions/43485569/installed-a-package-with-anaconda-cant-import-in-python
 """
 
 setup(
     name='bettermdptools',
     url='https://github.com/jlm429/bettermdptools',
-    version='0.5.5',
+    version='0.5.6',
     platforms=['Any'],
     license='New BSD',
     author='John Mansfield',
