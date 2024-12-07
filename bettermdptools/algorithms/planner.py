fd69ef8b2834c373a1f1232fcbff7bbc0197e127
Ethan Koch <ekoch8@gatech.edu>
2024-12-07 08:04:44 -0800

    make vectorized separate methdo

--- bettermdptools/algorithms/planner.py
+++ bettermdptools/algorithms/planner.py
@@ -57,6 +57,54 @@ class Planner:
         pi {lambda}, input state value, output action value:
             Policy mapping states to actions.
         """
+        V = np.zeros(len(self.P), dtype=np.float64)
+        V_track = np.zeros((n_iters, len(self.P)), dtype=np.float64)
+        i = 0
+        converged = False
+        while i < n_iters - 1 and not converged:
+            i += 1
+            Q = np.zeros((len(self.P), len(self.P[0])), dtype=np.float64)
+            for s in range(len(self.P)):
+                for a in range(len(self.P[s])):
+                    for prob, next_state, reward, done in self.P[s][a]:
+                        Q[s][a] += prob * (reward + gamma * V[next_state] * (not done))
+            if np.max(np.abs(V - np.max(Q, axis=1))) < theta:
+                converged = True
+            V = np.max(Q, axis=1)
+            V_track[i] = V
+        if not converged:
+            warnings.warn("Max iterations reached before convergence.  Check n_iters.")
+
+        pi = {s: a for s, a in enumerate(np.argmax(Q, axis=1))}
+        return V, V_track, pi
+
+    def value_iteration_vectorized(self, gamma=1.0, n_iters=1000, theta=1e-10):
+        """
+        PARAMETERS:
+
+        gamma {float}:
+            Discount factor
+
+        n_iters {int}:
+            Number of iterations
+
+        theta {float}:
+            Convergence criterion for value iteration.
+            State values are considered to be converged when the maximum difference between new and previous state values is less than theta.
+            Stops at n_iters or theta convergence - whichever comes first.
+
+
+        RETURNS:
+
+        V {numpy array}, shape(possible states):
+            State values array
+
+        V_track {numpy array}, shape(n_episodes, nS):
+            Log of V(s) for each iteration
+
+        pi {lambda}, input state value, output action value:
+            Policy mapping states to actions.
+        """
         S = len(self.P)
         A = len(self.P[0])
 
